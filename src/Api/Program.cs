4dcb671bfe29e1aebe3e270fee828bd67fc779f5
hieuvau198 <nhatduongfpt2000@gmail.com>
2025-01-23 03:00:54 +0700

    Create User CRUD

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,7 +1,9 @@
 using Application.Interfaces;
 using Application.Services;
 using Domain.Interfaces;
+using Infrastructure.Persistence;
 using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,9 +11,21 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Dependency Injection
-builder.Services.AddScoped<IHouseRepository, HouseRepository>();
-builder.Services.AddScoped<IHouseService, HouseService>();
+// Register the DbContext
+builder.Services.AddDbContext<SkincareDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Register DI for services such as Repositories, Application Services, etc
+
+    // House DI
+    builder.Services.AddScoped<IHouseRepository, HouseRepository>();
+    builder.Services.AddScoped<IHouseService, HouseService>();
+
+    // User DI
+    builder.Services.AddScoped<IUserRepository, UserRepository>();
+    builder.Services.AddScoped<IUserService, UserService>();
+
+
 
 var app = builder.Build();
 
@@ -20,6 +34,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Configure the HTTP request pipeline
 
 app.UseAuthorization();
 
