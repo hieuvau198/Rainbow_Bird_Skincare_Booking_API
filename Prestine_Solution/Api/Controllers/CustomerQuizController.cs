009f1bb59be45b694b2eaafa8d98bcf9065fec45
hieuvau198 <nhatduongfpt2000@gmail.com>
2025-02-16 17:10:17 +0700

    Create CustomerQuiz Api

---
+++ Prestine_Solution/Api/Controllers/CustomerQuizController.cs
@@ -0,0 +1,63 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerQuizController : ControllerBase
+    {
+        private readonly ICustomerQuizService _customerQuizService;
+
+        public CustomerQuizController(ICustomerQuizService customerQuizService)
+        {
+            _customerQuizService = customerQuizService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CustomerQuizDto>>> GetAllCustomerQuizzes()
+        {
+            return Ok(await _customerQuizService.GetAllCustomerQuizzesAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CustomerQuizDto>> GetCustomerQuizById(int id)
+        {
+            return Ok(await _customerQuizService.GetCustomerQuizByIdAsync(id));
+        }
+
+        [HttpGet("customer/{customerId}")]
+        public async Task<ActionResult<IEnumerable<CustomerQuizDto>>> GetCustomerQuizzesByCustomerId(int customerId)
+        {
+            return Ok(await _customerQuizService.GetCustomerQuizzesByCustomerIdAsync(customerId));
+        }
+
+        [HttpGet("customer/{customerId}/completed")]
+        public async Task<ActionResult<IEnumerable<CustomerQuizDto>>> GetCompletedQuizzesByCustomerId(int customerId)
+        {
+            return Ok(await _customerQuizService.GetCompletedQuizzesByCustomerIdAsync(customerId));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CustomerQuizDto>> StartQuiz(CreateCustomerQuizDto createDto)
+        {
+            return Ok(await _customerQuizService.StartQuizAsync(createDto));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateCustomerQuiz(int id, UpdateCustomerQuizDto updateDto)
+        {
+            await _customerQuizService.UpdateCustomerQuizAsync(id, updateDto);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteCustomerQuiz(int id)
+        {
+            await _customerQuizService.DeleteCustomerQuizAsync(id);
+            return Ok();
+        }
+    }
+}
---
+++ Prestine_Solution/Application/Interfaces/ICustomerQuizService.cs
@@ -0,0 +1,20 @@
+﻿using Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface ICustomerQuizService
+    {
+        Task<IEnumerable<CustomerQuizDto>> GetAllCustomerQuizzesAsync();
+        Task<CustomerQuizDto> GetCustomerQuizByIdAsync(int id);
+        Task<IEnumerable<CustomerQuizDto>> GetCustomerQuizzesByCustomerIdAsync(int customerId);
+        Task<CustomerQuizDto> StartQuizAsync(CreateCustomerQuizDto createDto);
+        Task UpdateCustomerQuizAsync(int id, UpdateCustomerQuizDto updateDto);
+        Task DeleteCustomerQuizAsync(int id);
+        Task<IEnumerable<CustomerQuizDto>> GetCompletedQuizzesByCustomerIdAsync(int customerId);
+    }
+}
